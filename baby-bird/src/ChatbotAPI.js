922566ea60d090b37e12a326152c015128d05789
turnipcow <nina@DESKTOP-5RTNPRD.localdomain>
2021-09-12 03:58:12 +1000

    added working chatbot :*

---
+++ baby-bird/src/ChatbotAPI.js
@@ -0,0 +1,28 @@
+const API = {
+  GetChatbotResponse: async message => {
+    let r = await postData(message);
+    return new Promise(function(resolve, reject) {
+      resolve(r.answers[0].answer); // .answers[0].answer
+    });
+  }
+};
+
+async function postData(message) {
+  let headersData = {
+    "Content-Type": "application/json",
+    "accept": "application/json",
+    "accept-encoding": "gzip, deflate",
+    "accept-language": "en-US,en;q=0.8",
+    "Authorization": "EndpointKey 43021727-483a-4885-bb3e-507ac2ff6393"
+  };
+
+  const response = await fetch("https://baby-bird-bot.azurewebsites.net/qnamaker/knowledgebases/50719c43-21a6-4f36-84eb-439090e98754/generateAnswer", {
+    method: "POST",
+    headers: headersData,
+    body: JSON.stringify({"question": message}),
+  });
+
+  return response.json();
+}
+
+export default API;
